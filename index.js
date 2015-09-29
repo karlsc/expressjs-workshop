08d91ab78561224e1216720a7936606d78f16340
Karl Soulière-Crépeau <karlysun@gmail.com>
2015-09-29 22:54:11 +0000

    Completed exercise 5 and added dependencies to package.json

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var url = require('url');
 
 var entries = {
   1: {
@@ -25,26 +26,57 @@ var entries = {
       {type: "home", address: "bill@scott.com"},
       {type: "work", address: "bscott@megacorp.com"}
     ]
+  },
+  4: {
+    firstName: "John",
+    lastName: "Lavoie",
+    emails: [
+      {type: "home", address: "john@lavoie.com"},
+      {type: "work", address: "jlavoie@megacorp.com"}
+    ]
+  },
+  5: {
+    firstName: "Samuel",
+    lastName: "Doe",
+    emails: [
+      {type: "home", address: "samuel@doe.com"},
+      {type: "work", address: "sdoe@megacorp.com"}
+    ]
+  },
+  6: {
+    firstName: "Dominic",
+    lastName: "Doe",
+    emails: [
+      {type: "home", address: "dominic@doe.com"},
+      {type: "work", address: "ddoe@megacorp.com"}
+    ]
   }
 };
 
-
-
-
-app.get('/entry/:entryId', function (req, res) {
+app.get('/entry/search', function (req, res) {
   
-  var entryId = req.params.entryId;
+  var url_parts = url.parse(req.url, true);
+  var query = url_parts.query;
+  var firstName = query.firstName;
+  var lastName = query.lastName;
+  var email = query.email;
+  var keysInEntries = Object.keys(entries);
+  var resultsFound = [];
   
-  if(entries[entryId]){
-    res.json(entries[entryId]);
+  for(var i = 1 ; i <= keysInEntries.length ; i++){
+    
+    if(firstName === entries[i].firstName || lastName === entries[i].lastName || email === entries[i].emails[0].address || email === entries[i].emails[1].address){
+      
+      resultsFound.push(entries[i].lastName+", "+entries[i].firstName+" | Email 1: "+entries[i].emails[0].address+" ("+entries[i].emails[0].type+") | Email 2: "+entries[i].emails[1].address+" ("+entries[i].emails[1].type+")");
+    } 
+  }
+  
+  if(!resultsFound){
+    res.send("Error: No entry found.");
   } else {
-    res.status(400).send('Bad request: Id selected unavailable.')
+    res.json(resultsFound);
   }
 });
-
-
-
-
 
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
 
