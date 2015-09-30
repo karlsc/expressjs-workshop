4224984141b78f5b386ebbf4bb09e9cceeaa29e2
Karl Soulière-Crépeau <karlysun@gmail.com>
2015-09-30 00:30:23 +0000

    Exercise 6 is done and updated package.json for new dependencies

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var url = require('url');
+var bodyParser = require('body-parser')
 
 var entries = {
   1: {
@@ -26,57 +27,24 @@ var entries = {
       {type: "home", address: "bill@scott.com"},
       {type: "work", address: "bscott@megacorp.com"}
     ]
-  },
-  4: {
-    firstName: "John",
-    lastName: "Lavoie",
-    emails: [
-      {type: "home", address: "john@lavoie.com"},
-      {type: "work", address: "jlavoie@megacorp.com"}
-    ]
-  },
-  5: {
-    firstName: "Samuel",
-    lastName: "Doe",
-    emails: [
-      {type: "home", address: "samuel@doe.com"},
-      {type: "work", address: "sdoe@megacorp.com"}
-    ]
-  },
-  6: {
-    firstName: "Dominic",
-    lastName: "Doe",
-    emails: [
-      {type: "home", address: "dominic@doe.com"},
-      {type: "work", address: "ddoe@megacorp.com"}
-    ]
   }
 };
 
-app.get('/entry/search', function (req, res) {
-  
-  var url_parts = url.parse(req.url, true);
-  var query = url_parts.query;
-  var firstName = query.firstName;
-  var lastName = query.lastName;
-  var email = query.email;
-  var keysInEntries = Object.keys(entries);
-  var resultsFound = [];
-  
-  for(var i = 1 ; i <= keysInEntries.length ; i++){
+app.use(bodyParser());
+
+app.post('/entry', function(req, res){
+
+    var keysInEntries = Object.keys(entries);
+    var keysInEntriesLength = keysInEntries.length;
     
-    if(firstName === entries[i].firstName || lastName === entries[i].lastName || email === entries[i].emails[0].address || email === entries[i].emails[1].address){
-      
-      resultsFound.push(entries[i].lastName+", "+entries[i].firstName+" | Email 1: "+entries[i].emails[0].address+" ("+entries[i].emails[0].type+") | Email 2: "+entries[i].emails[1].address+" ("+entries[i].emails[1].type+")");
-    } 
-  }
-  
-  if(!resultsFound){
-    res.send("Error: No entry found.");
-  } else {
-    res.json(resultsFound);
-  }
-});
+    keysInEntriesLength++;
+    
+    entries[keysInEntriesLength] = {firstName: req.body.firstName, lastName: req.body.lastName};
+    
+    res.send("ID #"+keysInEntriesLength+" = "+entries[keysInEntriesLength].lastName+", "+entries[keysInEntriesLength].firstName);
+    res.end();
+
+}); 
 
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
 
